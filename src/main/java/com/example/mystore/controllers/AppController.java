820969d6d6d5a50cd5891dbdbdf12cb305935220
Gustavo Aveiro <gustavo.aveiro03@gmail.com>
2018-05-31 20:05:47 -0300

    Changed controller.java

--- src/main/java/com/example/mystore/controllers/AppController.java
+++ src/main/java/com/example/mystore/controllers/AppController.java
@@ -3,6 +3,7 @@ package com.example.mystore.controllers;
 import java.math.BigInteger;
 
 import org.json.JSONArray;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.example.mystore.domain.DataBaseProvider;
 public class AppController {
 
 	@RequestMapping("product/list")
+	@CrossOrigin(origins = "http://http://172.245.190.232:8080")
 	public String getCompleteList() {
 		JSONArray jsonArray = new JSONArray();
 		DataBaseProvider dataBaseProvider = new DataBaseProvider();
@@ -27,6 +29,7 @@ public class AppController {
 	}
 
 	@RequestMapping(value = "product/by-id/", method = RequestMethod.GET)
+	@CrossOrigin(origins = "http://http://172.245.190.232:8080")
 	public String getProductBytId(@RequestParam("id") BigInteger id) {
 		JSONArray jsonArray = new JSONArray();
 		DataBaseProvider dataBaseProvider = new DataBaseProvider();
@@ -40,6 +43,7 @@ public class AppController {
 	}
 
 	@RequestMapping(value = "product/by-p_venue/", method = RequestMethod.GET)
+	@CrossOrigin(origins = "http://http://172.245.190.232:8080")
 	public String getProductBypVenue(@RequestParam("p_venue") String p_venue) {
 		JSONArray jsonArray = new JSONArray();
 		DataBaseProvider dataBaseProvider = new DataBaseProvider();
